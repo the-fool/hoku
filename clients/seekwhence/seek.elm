8b78c42e4e5c4767c1c144f480a97e011fda850a
thomas <sketchbang@gmail.com>
2017-09-28 23:21:25 -0500

    star trek colors

--- clients/seekwhence/seek.elm
+++ clients/seekwhence/seek.elm
@@ -1,18 +1,54 @@
 module Main exposing (..)
 
-import Html exposing (Html, program)
+import Html exposing (Html, program, div)
+import Html.Events exposing (onClick)
+import DynamicStyle exposing (hover, hover_)
 import Svg exposing (circle, line, svg, g, rect)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second, millisecond)
+import List.Extra exposing ((!!))
 
 
 main =
     program { init = init, view = view, update = update, subscriptions = subs }
 
 
+type Color
+    = Red
+    | Blue
+    | Green
+    | Yellow
+    | White
+
+
 type alias NoteBank =
-    { color : String
-    , notes : List Bool
+    { color : Color
+    , notes : Note
+    }
+
+
+type alias Note =
+    { do : Bool
+    , re : Bool
+    , mi : Bool
+    , fa : Bool
+    , sol : Bool
+    , la : Bool
+    , ti : Bool
+    , do2 : Bool
+    }
+
+
+initNotes : Note
+initNotes =
+    { do = False
+    , re = False
+    , mi = False
+    , fa = False
+    , sol = False
+    , la = False
+    , ti = False
+    , do2 = False
     }
 
 
@@ -29,42 +65,81 @@ type alias Model =
 
 -- VIEW
 
-steps = 16
+
+steps : Int
+steps =
+    16
+
 
 view : Model -> Html Msg
 view model =
     let
         angle =
-            turns <| (\x -> x / steps) <| toFloat <| model.step
+            turns <| toFloat model.step / toFloat steps
 
         handX =
-            toString (50 + 40 * cos (angle))
+            toString (10 + 9 * cos (angle))
 
         handY =
-            toString (50 + 40 * sin (angle))
+            toString (10 + 9 * sin (angle))
     in
-        svg [ viewBox "0 0 100 100", width "800px", height "500px" ]
-            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-            , g [ id "note-banks" ] (noteBanks model)
-            , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+        svg [ viewBox "0 0 200 100", width "1600px", height "800px" ]
+            [ rect [ x "0", y "0", width "200", height "100" ] []
+            , g [ transform "translate(20, 0)", id "note-banks" ] (noteBanks model)
+            , circle [ cx "10", cy "10", r "9", fill "#0B79CE" ] []
+            , line [ x1 "10", y1 "10", x2 handX, y2 handY, stroke "#023963" ] []
             ]
+
+
+toHexColor : Color -> String
+toHexColor color =
+    case color of
+        Red ->
+            "#a70d0d"
+
+        Blue ->
+            "#0099f6"
+
+        Green ->
+            "#00b844"
+
+        Yellow ->
+            "#ffc107"
+
+        White ->
+            "#fefefe"
+
+
+padding =
+    5
+
+
+addPadding =
+    (+) padding
 
 
 noteBanks model =
     let
         toBank i noteBank =
             List.indexedMap
-                (\j _ ->
+                (\j ac ->
                     rect
-                        [ width "10"
-                        , height "10"
-                        , x << toString << (*) 11 <| i
-                        , y << toString << (*) 11 <| j
-                        ]
+                        ([ fill <| toHexColor noteBank.color
+                         , rx "2"
+                         , ry "2"
+                         , width "9"
+                         , height "6"
+                         , x << toString << addPadding << (*) 11 <| i
+                         , y << toString << addPadding << (*) 8 <| j
+                         , onClick <| Select i j
+                         , stroke "chartreuse"
+                         ]
+                            ++ (hover [ ( "stroke-width", if (ac noteBank.notes) then "1px" else "0px" , "1px" ), ( "cursor", "normal", "pointer" ) ])
+                        )
                         []
                 )
-                noteBank.notes
-                |> g [ id noteBank.color, class "note-bank" ]
+                [.do, .re, .mi, .fa, .sol, .la, .ti, .do2]
+                |> g []
     in
         List.indexedMap toBank model.noteBanks
 
@@ -75,6 +150,7 @@ noteBanks model =
 
 type Msg
     = Tick
+    | Select Int Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -83,13 +159,16 @@ update msg model =
         Tick ->
             ( { model | step = (model.step + 1) % steps }, Cmd.none )
 
+        Select i j ->
+            ( model, Cmd.none )
+
 
 init : ( Model, Cmd Msg )
 init =
     ( { step = 0
       , noteBanks =
-            [ "red", "blue", "green", "yellow" ]
-                |> List.map (\x -> { color = x, notes = List.repeat 6 False })
+            [ Red, Green, Blue, Yellow, White ]
+                |> List.map (\x -> { color = x, notes = initNotes })
       }
     , Cmd.none
     )
