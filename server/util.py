b819ad3d6e252b348e212e3f34cbccf3e683cc7f
thomas <sketchbang@gmail.com>
2017-11-01 20:09:25 -0500

    dead code

--- server/util.py
+++ server/util.py
@@ -1,11 +1,4 @@
 def scale(value, d_min, d_max, r_min, r_max):
-    # Figure out how 'wide' each range is
-    r_span = r_max - r_min
-    d_span = d_max - d_min
-
-    # Convert the left range into a 0-1 range (float)
-    value_scaled = float(value - r_min) / float(r_span)
-
     return float(r_max - r_min) * (value - d_min) / (d_max - d_min) + r_min
 
 
