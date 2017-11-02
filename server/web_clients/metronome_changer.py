e5952aff90b4fcb5253a05f652a6839a7cfdcb72
thomas <sketchbang@gmail.com>
2017-11-01 21:59:54 -0500

    metro maker

--- server/web_clients/metronome_changer.py
+++ server/web_clients/metronome_changer.py
@@ -2,15 +2,20 @@ from server.observable import observable_factory
 import json
 
 
+def msg_maker(bpm: int):
+    return json.dumps({'current_bpm': bpm})
+
+
 def metronome_changer_factory(bpm_queue, bpm):
-    obs, emit = observable_factory(bpm)
+    msg = msg_maker(bpm)
+    obs, emit = observable_factory(msg)
 
     async def ws_consumer(kind, payload, uuid):
         nonlocal bpm_queue
 
         if kind == 'change':
             await bpm_queue.put(payload)
-            msg = json.dumps({'current_bpm': payload})
+            msg = msg_maker(payload)
             await emit(msg)
 
     return obs, ws_consumer
