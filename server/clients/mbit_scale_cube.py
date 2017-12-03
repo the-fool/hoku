56f38531a08da36dce0f504b394476529275f1cf
thomas <sketchbang@gmail.com>
2017-12-03 16:52:49 -0600

    do not use midi worker anymore

--- server/clients/mbit_scale_cube.py
+++ server/clients/mbit_scale_cube.py
@@ -3,4 +3,8 @@ class MbitScaleCube:
         self.scale_cube = scale_cube
 
     def on_uart(evt):
+        """
+        Receives a uart message,
+        which triggers a method on the scale cube
+        """
         pass
